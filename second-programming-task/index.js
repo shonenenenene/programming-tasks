a5dacacf25da276774c9cc73185e858dc66f4de0
shonenenenene <danya.sytaev@gmail.com>
2022-07-31 15:59:39 +0300

    final

--- second-programming-task/index.js
+++ second-programming-task/index.js
@@ -1,38 +1,44 @@
-function arrayTransform(arr) {
-    arr.sort((a, b) => a - b)
+function arrayTransformer(arr) {
+    arr.sort((a, b) => a - b) // Сортировка массива в порядке возрастания
 
-    let stringedArr = arr.map((e) => e.toString())
-    let n = 0
+    let stringedArr = arr.map((e) => e.toString()) // Преобразование элементов массива в строки для удобства работы
+    let n = 0 // Начальное значение индекса для поиска совпадений
 
-    function finder(arr) {
-        const arrayedArr = arr.map((e) => Array.from(e))
-        console.log(arrayedArr)
-        const arrFromFirstEl = arrayedArr[n]
-        let indexesToDelete = new Set()
+    function arrayConverter(arr) {
+        const arrayedArr = arr.map((e) => Array.from(e)) // Преобразование элементов массива в массивы символов для последовательного сравнения
+        const firstElement = arrayedArr[n]
+        let indexesToDelete = new Set() // Коллекция индексов повторяющихся значений
         arrayedArr.forEach((e, i) => {
-            arrFromFirstEl.forEach((element, index) => {
+            firstElement.forEach((element, index) => {
                 if (e.includes(element)) {
                     indexesToDelete.add(i)
                 }
             })
         })
-        console.log(indexesToDelete)
+        // Проверка, что в массиве есть элементы с повторяющимися цифрами
         if (indexesToDelete.size > 1) {
-            arr = arr.filter((e, i) => ![...indexesToDelete].includes(i))
-            console.log(arr)
+            arr = arr.filter((e, i) => ![...indexesToDelete].includes(i)) // Если есть, фильтруем их, затем обнуляем счетчик и прогоняем полученный массив снова
             n = 0
-            return finder(arr)
+            return arrayConverter(arr)
+            // Если повторяющихся цифр нет, берем следующий элемент и снова ищем совпадения
         } else {
             if (n === arr.length - 1) {
-                console.log(arr)
+                // Выводим в консоль результат и возвращаем его если мы уже прошлись по всему массиву
+                console.log(arr.map((e) => +e))
                 return arr
             }
             n++
-            return finder(arr)
+            return arrayConverter(arr)
         }
     }
 
-    finder(stringedArr)
+    const semiFinalArr = arrayConverter(stringedArr)
+    const reversedArr = semiFinalArr.map((e) =>
+        Array.from(e).reverse().join('')
+    )
+    const powedArr = reversedArr.map((e) => Math.pow(+e, 2).toString())
+    n = 0
+    return arrayConverter(powedArr) // После реверса и возведения в квадрат снова прогоняем массив через функцию и получаем результат
 }
 
-console.log(arrayTransform([41, 55, 61, 1, 8, 27, 37, 39]))
+arrayTransformer([41, 55, 61, 1, 8, 27, 37, 39])
